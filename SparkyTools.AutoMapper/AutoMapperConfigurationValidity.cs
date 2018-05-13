8e85d49d37de9983893570b37ed908c82a08bada
Brian Schroer <brianschroer@gmail.com>
2018-05-13 16:19:09 -0500

    Added ExceptionHelper

--- SparkyTools.AutoMapper/AutoMapperConfigurationValidity.cs
+++ SparkyTools.AutoMapper/AutoMapperConfigurationValidity.cs
@@ -83,53 +83,15 @@ namespace SparkyTools.AutoMapper
             }
             catch (AutoMapperConfigurationException ex)
             {
-                string improvedMessage = ImproveExceptionMessage(ex);
+                string improvedMessage = ExceptionHelper.AddUnmappedPropertyHelperInfo(ex);
+
                 if (improvedMessage == ex.Message)
                 {
                     throw;
                 }
+
                 throw new AutoMapperConfigurationException(improvedMessage);
             }
-        }
-
-        private static string ImproveExceptionMessage(AutoMapperConfigurationException ex)
-        {
-            string message = ex.Message;
-
-            try
-            {
-
-                var sb = new StringBuilder();
-
-                foreach (AutoMapperConfigurationException.TypeMapConfigErrors errors in ex.Errors)
-                {
-                    string[] unmappedPropertyNames = errors.UnmappedPropertyNames;
-                    if (unmappedPropertyNames.Length > 0)
-                    {
-                        TypeMap typeMap = errors.TypeMap;
-                        string typeString = $"<{typeMap.SourceType.Name}, {typeMap.DestinationType.Name}>";
-
-                        sb.AppendLine(
-                            $"\n\tcfg.CreateMap<{typeMap.SourceType.Name}, {typeMap.DestinationType.Name}>()\n\n"
-                            + $"\t\t.IgnoreMember{(unmappedPropertyNames.Length == 1 ? "" : "s")}(dest => dest."
-                            + string.Join(", dest => dest.", unmappedPropertyNames)
-                            + ");");
-                    }
-                }
-
-                if (sb.Length > 0)
-                {
-                    string separator = new string('_', 100);
-                    sb.Insert(0,
-                        $"\n{separator}\nIf you want to ignore the properties shown above, you can cut/paste the following code stub(s):\n");
-                    message = ex.Message + sb;
-                }
-            }
-            catch
-            {
-            }
-
-            return message;
         }
     }
 }
